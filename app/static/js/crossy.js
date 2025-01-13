ade461a72b00c7c3e384be93fb79ba61179fc76e
klin50 <kevinl713@nycstudents.net>
2025-01-12 21:05:41 -0500

    changed everything to canvas

--- app/static/js/crossy.js
+++ app/static/js/crossy.js
@@ -1,38 +1,38 @@
 //Gets the game element/box
-let game = document.getElementById("game");
-//Gets the height of the game
-let height = game.offsetHeight;
-let LaneSize = Math.floor(height/12);
-//Gets the coordinates with top left corner as origin
-let pos = game.getBoundingClientRect();
-let carW = Math.floor(game.offsetWidth/15);
-let rightSpawn = pos.right;
-let leftSpawn = pos.left;
+let canvas = document.getElementById("game");
+let ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let LaneSize = Math.floor(canvas.height/12);
+let carW = Math.floor(canvas.width / 15);
 let minSpawn = 500;
 let maxSpawn = 1000;
 let score = 0;
 let playing = true;
 let animationId;
-var chicken;
+let Chicken;
 function Lane(ycor){
   this.type = Math.random()*3;
   this.direction = Math.random();
   this.y = ycor;
-  this.el = null;
   this.cars = [];
-  this.carInterval = setInterval(() => makeCar(this.cars,new car(this)),(Math.random() * (maxSpawn - minSpawn) + minSpawn));
+  if(this.type > 2){
+    this.carInterval = null;
+  }
+  else{
+    this.carInterval = setInterval(() => makeCar(this.cars,new car(this)),(Math.random() * (maxSpawn - minSpawn) + minSpawn));
+  }
 }
 function car(lane){
   if(lane.direction > 0.5){
-    this.x = rightSpawn + Math.random()*100;
+    this.x = canvas.width + Math.random()*100;
     this.speed = -10;
   }
   else{
-    this.x = leftSpawn - Math.random()*100;
+    this.x = - Math.random()*100;
     this.speed = 10;
   }
   this.y = lane.y;
-  this.el = null;
 }
 function main(){
   var lanes = [];
@@ -40,131 +40,98 @@ function main(){
 }
 function setup(arr){
   var start = document.createElement("BUTTON");
+  var rect = canvas.getBoundingClientRect();
   start.style.position = "absolute";
-  start.style.left = pos.left + 3*pos.width/8 + "px";
-  start.style.top = pos.bottom-pos.height/2 + "px";
-  start.style.width = pos.width/4+"px";
-  start.style.height = pos.height/4+"px";
+  start.style.left = rect.left+rect.width/2+"px";
+  start.style.top = rect.top + rect.height/2 + "px";
+  start.style.width = canvas.width/4+"px";
+  start.style.height = canvas.height/4+"px";
   start.textContent = "START";
   start.style.border = "4px";
-  start.onclick = function(){initialize(arr);};
-  game.append(start);
+  start.onclick = function(){document.body.removeChild(start);initialize(arr);};
+  document.body.append(start);
 }
 function makeCar(arr, car){
-  var newCar = document.createElement("div");
-  newCar.style.position = "absolute";
-  newCar.style.left = car.x+"px";
-  newCar.style.top = car.y+"px";
-  newCar.style.width = carW+"px";
-  newCar.style.height = LaneSize+"px";
-  newCar.style.backgroundColor = "purple";
-  car.el = newCar;
   arr.push(car);
-  game.append(newCar);
 }
 function initialize(arr){
-  chicken = new chicken();
-  for(var i = 0; i < Math.ceil(height/LaneSize);i ++){
-    arr.push(new Lane(pos.bottom-LaneSize*(i+1)));
-    addElement(arr,i);
+  Chicken = new chicken();
+  for(var i = 0; i < Math.ceil(canvas.height/LaneSize);i ++){
+    arr.push(new Lane(canvas.height-LaneSize*(i+1)));
   }
   document.addEventListener("keydown", function(event) {
     if(playing){
       if(event.key === "w" || event.key === "ArrowUp") {
-        chicken.y -= LaneSize;
+        Chicken.y -= LaneSize;
         score ++;
       }
       if(event.key === "a" || event.key === "ArrowLeft") {
-        chicken.x -= LaneSize;
+        Chicken.x -= LaneSize;
       }
       if(event.key === "d" || event.key === "ArrowRight") {
-        chicken.x += LaneSize;
+        Chicken.x += LaneSize;
       }
-      chicken.el.style.top = chicken.y + "px";
-      chicken.el.style.left = chicken.x + "px";
       }
     });
   animate(arr);
+}
+function drawBackground(arr){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].type > 2) {
+      ctx.fillStyle = "lightgreen";
+    } else if (arr[i].type > 1) {
+      ctx.fillStyle = "lightblue";
+    } else {
+      ctx.fillStyle = "gray";
+    }
+    ctx.fillRect(0, arr[i].y, canvas.width, LaneSize);
+  }
 }
 //Increments the lane y-cor and updates the actual element on html
 function backgroundMove(arr){
   for(var i = 0; i < arr.length; i ++){
     arr[i].y ++;
-    arr[i].el.style.top = arr[i].y + "px";
     modCars(arr[i].cars);
   }
-  chicken.y ++;
-  chicken.el.style.top = chicken.y + "px";
+  Chicken.y ++;
 }
 function modCars(arr){
   if (arr.length === 0 || !playing) return;
   if(arr[0].speed < 0){
-    var bound = pos.left;
+    var bound = 0;
     for (var i = arr.length - 1; i >= 0; i--){
       if(arr[i].x+carW < bound){
-        game.removeChild(arr[i].el);
         arr.splice(i,1);
       }
       else{
         arr[i].x += arr[i].speed;
         arr[i].y += 1;
-        arr[i].el.style.left = arr[i].x+"px";
-        arr[i].el.style.top = arr[i].y+"px";
       }
     }
   }
   else{
-    var bound = pos.right;
+    var bound = canvas.right;
     for (var i = arr.length - 1; i >= 0; i--){
       if(arr[i].x > bound){
-        game.removeChild(arr[i].el);
         arr.splice(i,1);
       }
       else{
         arr[i].x += arr[i].speed;
         arr[i].y += 1;
-        arr[i].el.style.left = arr[i].x+"px";
-        arr[i].el.style.top = arr[i].y+"px";
       }
     }
   }
 }
 //Adds and removes lanes
 function modLanes(arr){
-  if(arr[arr.length-1].y >= pos.top){
+  if(arr[arr.length-1].y >= 0){
     arr.push(new Lane(arr[arr.length-1].y-LaneSize));
-    addElement(arr,arr.length-1);
   }
-  if(arr[0].y>= pos.bottom){
+  if(arr[0].y>= canvas.height){
     clearInterval(arr[0].carInterval);
-    for(var i = 0; i < arr[0].cars.length; i ++){
-      game.removeChild(arr[0].cars[i].el);
-    }
     arr[0].cars = [];
-    game.removeChild(arr[0].el);
     arr.shift();
   }
-}
-//Creates a lane element and adds it to game
-//Manually setting coords might not be good but it works
-function addElement(arr,i){
-  var image = document.createElement("div");
-  image.style.position = "absolute";
-  image.style.left = pos.left+"px";
-  image.style.top = arr[i].y+"px";
-  image.style.width = game.offsetWidth+"px";
-  image.style.height = LaneSize+"px";
-  if(arr[i].type > 2){
-    image.style.backgroundColor = "lightgreen";
-  }
-  if(arr[i].type > 1 && arr[i].type < 2){
-    image.style.backgroundColor = "lightblue";
-  }
-  if(arr[i].type > 0 && arr[i].type < 1){
-    image.style.backgroundColor = "gray";
-  }
-  arr[i].el = image;
-  game.append(image);
 }
 //Basically a loop that animates the screen
 function animate(arr){
@@ -175,34 +142,44 @@ function animate(arr){
     }
     return;
   }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   backgroundMove(arr);
   modLanes(arr);
+  drawAll(arr);
   for(var i = 0; i < arr.length; i ++){
-    if(arr[i].y == chicken.y){
+    if(arr[i].y == Chicken.y){
       collision(arr[i].cars);
     }
   }
   animationId = requestAnimationFrame(function() { animate(arr); });
 }
 function chicken(){
-  this.x = pos.left + pos.width/2 - LaneSize/4;
-  this.y = pos.bottom - LaneSize;
-  this.el = document.createElement("div");
-  this.el.style.position = "absolute";
-  this.el.style.left = this.x+"px";
-  this.el.style.top = this.y+"px";
-  this.el.style.width = LaneSize/2+"px";
-  this.el.style.height = LaneSize/2+"px";
-  this.el.style.backgroundColor = "white";
-  this.el.style.zIndex = 1;
-  game.append(this.el);
+  this.x = canvas.width/2 - LaneSize/4;
+  this.y = canvas.height - LaneSize;
+}
+function drawChicken() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(Chicken.x, Chicken.y, LaneSize / 2, LaneSize / 2);
+}
+function drawCars(arr) {
+  ctx.fillStyle = "purple";
+  for (let lane of arr) {
+    for (let car of lane.cars) {
+      ctx.fillRect(car.x, car.y, carW, LaneSize);
+    }
+  }
 }
 function collision(arr){
   for(var i = 0; i < arr.length; i ++){
-    if((chicken.x > arr[i].x && chicken.x < arr[i].x + carW) || (chicken.x + LaneSize/2 > arr[i].x && chicken.x + LaneSize/2 < arr[i].x + carW)){
+    if((Chicken.x > arr[i].x && Chicken.x < arr[i].x + carW) || (Chicken.x + LaneSize/2 > arr[i].x && Chicken.x + LaneSize/2 < arr[i].x + carW)){
       playing = false;
       return;
     }
   }
+}
+function drawAll(arr){
+  drawBackground(arr);
+  drawCars(arr);
+  drawChicken();
 }
 main()
